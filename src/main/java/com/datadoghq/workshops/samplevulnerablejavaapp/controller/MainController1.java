47432f2cb0272d2991a99901f48df29a531482b2
sec1drawat <155938767+sec1drawat@users.noreply.github.com>
2026-03-03 10:39:54 +0530

    fix: apply SAST remediation to src/main/java/com/datadoghq/workshops/samplevulnerablejavaapp/controller/MainController1.java

--- src/main/java/com/datadoghq/workshops/samplevulnerablejavaapp/controller/MainController1.java
+++ src/main/java/com/datadoghq/workshops/samplevulnerablejavaapp/controller/MainController1.java
@@ -16,6 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.owasp.encoder.Encode;
 
 @RestController
 public class MainController {
@@ -50,7 +51,8 @@ public class MainController {
   public ResponseEntity<String> testWebsite(@RequestBody WebsiteTestRequest request) {
     log.info("Testing website " + request.url);
     String result = websiteTestService.testWebsite(request);
-    return new ResponseEntity<>(result, HttpStatus.OK);
+    String safeResult = Encode.forHtml(result);
+    return new ResponseEntity<>(safeResult, HttpStatus.OK);
   }
 
   @RequestMapping(method=RequestMethod.POST, value="/view-file", consumes="application/json")
