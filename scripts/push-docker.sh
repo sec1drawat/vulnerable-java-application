30bf24e440c822566288861bd4dc1904de6144d2
rahuldarekar222 <139219289+rahuldarekar222@users.noreply.github.com>
2026-03-04 17:20:59 +0530

    fix: apply SAST remediation to 5 files (3/5): scripts/push-docker.sh

--- scripts/push-docker.sh
+++ scripts/push-docker.sh
@@ -9,7 +9,7 @@ fi
 
 aws ecr create-repository --repository-name $REPO_NAME >/dev/null 2>&1 || true
 REPO_URL=$(aws ecr describe-repositories --repository-names  domain-tester-service | jq -r '.repositories[0].repositoryUri')
-docker tag domain-tester-service $REPO_URL
+docker tag domain-tester-service "$REPO_URL"
 
 REGISTRY_URL=$(echo $REPO_URL | cut -d/ -f1)
 aws ecr get-login-password | docker login --username AWS --password-stdin $REGISTRY_URL
